60fa12341cb4bf5241ca262cab9ec5f1616e57b9
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2019-07-16 13:22:20 -0300

    add RNGestureHandlerEnabledRootView codes add the key for the android project

--- android/app/src/main/java/com/rtnflixreactnative/MainActivity.java
+++ android/app/src/main/java/com/rtnflixreactnative/MainActivity.java
@@ -1,6 +1,10 @@
 package com.rtnflixreactnative;
 
 import com.facebook.react.ReactActivity;
+// Importações adicionadas
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +15,16 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "RtNflixReactNative";
+    }
+
+    // Método adicionado
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+      return new ReactActivityDelegate(this, getMainComponentName()) {
+        @Override
+        protected ReactRootView createRootView() {
+          return new RNGestureHandlerEnabledRootView(MainActivity.this);
+        }
+      };
     }
 }
