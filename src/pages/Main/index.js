1c57d0850ec00128201a79248b9656123aa2cfd8
Rodrigo Toledo <rodrigo.toledo.dev@gmail.com>
2019-07-17 19:31:02 -0300

    styles for main page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import { MaxContainer, HeaderApplication, GroupMenu, MenuItem } from './styles';
+
 import {
-  Text, Image, StyleSheet, Dimensions, ImageBackground, StatusBar,
+  Text, Image, StyleSheet, Dimensions, View, StatusBar,
 } from 'react-native';
 
 const styles = StyleSheet.create({
@@ -34,14 +36,19 @@ const styles = StyleSheet.create({
 });
 
 const Main = () => (
-  <ImageBackground
-    source={{
-      uri: 'https://s3-sa-east-1.amazonaws.com/rocketseat-cdn/background.png',
-    }}
-    style={styles.container}
-    resizeMode="cover"
-  >
-    <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+  <MaxContainer>
+    <StatusBar barStyle="light-content" backgroundColor="#212529" />
+    <HeaderApplication>
+      RT NFlix ReactNative
+    </HeaderApplication>
+    <GroupMenu>
+      <MenuItem>
+        <Text>Teste</Text>
+      </MenuItem>
+      <MenuItem>
+        <Text>Teste</Text>
+      </MenuItem>
+    </GroupMenu>
     <Image
       source={{
         uri: 'https://s3-sa-east-1.amazonaws.com/rocketseat-cdn/rocketseat_logo.png',
@@ -53,7 +60,7 @@ const Main = () => (
     <Text style={styles.instructions}>Essa é a tela principal da sua aplicação =)</Text>
     <Text style={styles.instructions}>Você pode editar a tela no arquivo:</Text>
     <Text style={[styles.instructions, styles.fileName]}>src/pages/Main/index.js</Text>
-  </ImageBackground>
+  </MaxContainer>
 );
 
 export default Main;
---
+++ src/pages/Main/styles.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components/native';
+
+export const HeaderApplication = styled.Text`
+  color: #fed136;
+  font-family: Arial;
+  font-size: 20px;
+  align-self: center;
+`;
+
+export const MaxContainer = styled.ScrollView`
+  background: #212529;
+  height: 100%;
+  padding: 10px;
+  display: flex;
+`;
+
+export const GroupMenu = styled.View`
+  width: 100%;
+  margin-top: 5px;
+  border-radius: 5px;
+  align-items: center;
+  align-content: space-between;
+  display: flex;
+`;
+
+export const MenuItem = styled.View`
+  background: #fcfcfc;
+  width: 80px;
+`;
